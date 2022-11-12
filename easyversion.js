7f7acc6b7505ac6df4557dd707a126dc1a1e8a1c
elliotcross <114322766+elliotcross@users.noreply.github.com>
2022-11-12 15:48:28 -0800

    Update easyversion.js

--- easyversion.js
+++ easyversion.js
@@ -9,6 +9,8 @@ function validateForm(form)
 	let h = document.forms["myForm"]["Password"].value;
 	let k = document.forms["myForm"]["Address"].value;
 	let m = document.forms["myForm"]["Country"].value;
+	let s = document.forms["myForm"]["State"].value;
+	let z = document.forms["myForm"]["Zipcode"].value;
 	if (x === "" || y === "" || z === "" || f === "" || g === "" || h === "" || k === "" || m === "-1") {
 		alert("Error: All fields are required");
 		return false;
@@ -50,17 +52,41 @@ function validateForm(form)
 
 //validation fails if phone is non-numeric characters or more than 15 characters
 	if (isNaN(f) || f.length >15 || phone===null) {
-		alert("Error: Phone must contain numbers only, no more than 15 characters");
+		alert("Error: Phone must contain numbers only, no more than 15 digits");
 		return false;
 	}
 	
 //validation fails if username is longer than 12 characters
+	if (g.length > 12) {
+		alert("Error: Username cannot be more than 12 characters");
+		return false;
+	}
 	
 //validation fails if password is longer than 7 characters
+	if (h.length > 7) {
+		alert("Error: Password cannot be more than 7 characters");
+		return false;
+	}
 	
 //State required if Country is USA
+	if (m === "011" && s === "-1") {
+		alert("Please choose a state");
+		return false;
+	}
 	
 //Zipcode required if Country is USA. Zipcode max 5 digits.
+	if (m === "011" && z === "") {
+		alert("Zipcode required");
+		return false;
+	}
+	if (m === "011" && isNaN(z)) {
+		alert("Zipcode must be numbers only");
+		return false;
+	}
+	if (m === "011" && z.length != 5) {
+		alert("Zipcode must be 5 digits long");
+		return false;
+	}
 
 	else return true;
 }
